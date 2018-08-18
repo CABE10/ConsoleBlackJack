a9aaa1f3e6e311e816afd2f39d7d1b8bbfa6e5ff
Cabe <cabe@rocketmail.com>
2018-08-17 22:00:11 -0600

    Added Test Project

---
+++ BlackJack_Tests/DeckServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackJack_Tests
+{
+    [TestClass]
+    public class DeckServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
